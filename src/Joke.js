329649943002d7cc487d9fd4acf42919a570b564
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-06-25 23:00:05 +0200

    linked components + starterCode

---
+++ src/Joke.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import "./Joke.css"
+
+class Joke extends Component {
+    render() {
+        return( <p>This is a joke!</p> )
+    }
+}
+
+export default Joke;
