9b8cb1c93009db8a6dc0d06f42b827a38e5e3886
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-06-28 20:57:00 +0200

    first idea of a joke component

--- src/Joke.js
+++ src/Joke.js
@@ -3,7 +3,16 @@ import "./Joke.css"
 
 class Joke extends Component {
     render() {
-        return( <p>This is a joke!</p> )
+        return (
+            <li className="Joke">
+                <div className="Joke-rating">
+                    <button></button>
+                    <p>Score: display</p>
+                    <button></button>
+                </div>
+                Here Comes the Joke
+            </li>
+         )
     }
 }
 
