cc42c714556a1004dbbe55e3f3211517c5c7749b
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-06-30 20:29:18 +0200

    made rating available

--- src/Joke.js
+++ src/Joke.js
@@ -1,20 +1,33 @@
-import React, { Component } from 'react'
-import "./Joke.css"
+import React, { Component } from 'react';
+import './Joke.css';
 
 class Joke extends Component {
-    render() {
-        const {text, id, rating} = this.props
-        return (
-            <li className="Joke">
-                <div className="Joke-rating">
-                    <button></button>
-                    <p>{rating}</p>
-                    <button></button>
-                </div>
-                {text}
-            </li>
-         )
-    }
+	constructor(props) {
+		super(props);
+		this.handleUpvote = this.handleUpvote.bind(this);
+		this.handleDownvote = this.handleDownvote.bind(this);
+	}
+
+	handleUpvote(evt) {
+		this.props.changeRating(this.props.id, +1);
+	}
+	handleDownvote(evt) {
+		this.props.changeRating(this.props.id, -1);
+	}
+
+	render() {
+		const { text, rating } = this.props;
+		return (
+			<li className='Joke'>
+				<div className='Joke-rating'>
+					<button className='Joke-btn fas fa-arrow-up' onClick={this.handleUpvote}></button>
+					<p className="Joke-score">{rating}</p>
+					<button className='Joke-btn fas fa-arrow-down' onClick={this.handleDownvote}></button>
+				</div>
+				<div className="Joke-text"><p>{text}</p></div>
+			</li>
+		);
+	}
 }
 
 export default Joke;
