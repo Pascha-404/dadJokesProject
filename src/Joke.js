00d0eb84bf61ec9730254c123020426c702af091
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-07-06 17:30:10 +0200

    changing border/face based on rating

--- src/Joke.js
+++ src/Joke.js
@@ -17,14 +17,33 @@ class Joke extends Component {
 
 	render() {
 		const { text, rating } = this.props;
+		let scoreBorder = { borderColor: "grey" }
+		let faceClass = "Joke-face far fa-meh-blank"
+		if (rating < 0) {
+			scoreBorder = { borderColor: "red" }
+			faceClass = 'Joke-face far fa-meh-rolling-eyes';
+		}
+		if (rating > 0) {
+			scoreBorder = { borderColor: "orange" }
+			faceClass = 'Joke-face far fa-laugh';
+		}
+		if (rating > 4) {
+			scoreBorder = { borderColor: "yellow" }
+			faceClass = "Joke-face far fa-laugh-beam"
+		}
+		if (rating > 8) {
+			scoreBorder = { borderColor: "lightgreen" }
+			faceClass = "Joke-face far fa-grin-squint-tears"
+		}
 		return (
 			<li className='Joke'>
 				<div className='Joke-rating'>
 					<button className='Joke-btn fas fa-arrow-up' onClick={this.handleUpvote}></button>
-					<p className="Joke-score">{rating}</p>
+					<p className="Joke-score" style={scoreBorder}>{rating}</p>
 					<button className='Joke-btn fas fa-arrow-down' onClick={this.handleDownvote}></button>
 				</div>
 				<div className="Joke-text"><p>{text}</p></div>
+				<div className={faceClass}></div>
 			</li>
 		);
 	}
